913feec33034977404fbe9a3d0f03fe836582aa0
abhinrustagi <rustagi.abhin@gmail.com>
2023-02-07 14:32:10 +0530

    chore: add husky & conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	formatter: '@commitlint/format',
+	helpUrl:
+		'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+}
